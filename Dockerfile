ff5c23228ca2586be13eb7c5a6a83fd5897e7c8d
Ross Hendry <rhendry@googlemail.com>
2016-11-27 18:51:52 +0000

    Initial commit
    A webhook listener which downloads and deploys the CSV files on a push.

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+FROM mhart/alpine-node:6.1.0
+MAINTAINER Ross Hendry "rhendry@googlemail.com"
+
+ENV HASH_SECRET='not really this'
+
+ADD . /opt/apps/ace-volume-updater
+WORKDIR /opt/apps/ace-volume-updater
+
+RUN npm install && npm cache clean
+EXPOSE 7777
+CMD ["npm", "start"]
---
+++ index.js
@@ -0,0 +1,50 @@
+const http = require('http'),
+  request = require('request'),
+  AdmZip = require('adm-zip'),
+  path = require('path'),
+  copydir = require('copy-dir'),
+  temp = require('temp').track(),
+  createHandler = require('github-webhook-handler'),
+  secret = process.env.HASH_SECRET || 'f4ker',
+  handler = createHandler({
+    path: '/hooks',
+    secret: secret
+  });
+
+http.createServer((req, res) => {
+  handler(req, res, (err) => {
+    res.statusCode = 404;
+    res.end('no such location');
+  });
+}).listen(7777);
+
+handler.on('err', function(err) {
+  console.error('Error :', err);
+});
+
+handler.on('push', downloadAndUnzip);
+
+function downloadAndUnzip() {
+  console.log('Event received');
+  var stream = temp.createWriteStream();
+
+  request('https://github.com/chooban/ace-data-container/archive/master.zip')
+    .pipe(stream)
+    .on('close', unzipAndCopy);
+
+  function unzipAndCopy() {
+    const zip = new AdmZip(stream.path);
+    temp.mkdir('contents', (err, dirPath) => {
+      if (err) {
+        console.error('Error unzipping download:', err);
+        return;
+      }
+
+      zip.extractAllTo(dirPath, true);
+      copydir.sync(dirPath + '/ace-data-container-master/data/', '/data/', (stat, filepath, filename) => {
+        return (stat ==='file' && path.extname(filepath) === '.csv');
+      });
+      temp.cleanup();
+    });
+  }
+}
